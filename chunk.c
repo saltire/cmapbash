d10048569448fb2704806c20ebd61a5d3945608c
saltire sable <x@saltiresable.com>
2014-04-13 18:23:54 -0400

    Add licence and copyright info

--- chunk.c
+++ chunk.c
@@ -1,3 +1,22 @@
+/*
+	cmapbash - a simple Minecraft map renderer written in C.
+	© 2014 saltire sable, x@saltiresable.com
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,7 +35,7 @@
 #define HEIGHT_MIDPOINT 0.3 // midpoint of height shading gradient
 #define HEIGHT_CONTRAST 0.7 // amount of contrast for height shading
 #define NIGHT_AMBIENCE 0.2 // base light level for night renders
-#define CONTOUR_SHADING 0.125 // amoutn of contrast for topographical highlights and shadows
+#define CONTOUR_SHADING 0.125 // amount of contrast for topographical highlights and shadows
 
 
 static void copy_section_bytes(nbt_node* section, char* name, unsigned char* data, int8_t y)
